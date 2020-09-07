c930624c6ee149e7c5d5f13da84f40545954dc2a
Doni Setiawan <donisetiawan.050@gmail.com>
2020-09-07 16:53:10 +0700

    Contacts

---
+++ screens/Contacts.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import { fetchContacts } from '../utils/api';
+import ContactListItem from '../components/ContactListItem';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
+
+const keyExtractor = ({ phone }) => phone;
+
+class Contacts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      loading: true,
+      error: false,
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const contacts = await fetchContacts();
+
+      this.setState({
+        contacts,
+        loading: false,
+        error: false,
+      });
+    } catch (e) {
+      this.setState({
+        loading: false,
+        error: true,
+      });
+    }
+  }
+
+  renderContact = ({ item }) => {
+    const { name, avatar, phone } = item;
+
+    return (
+      <ContactListItem
+        name={name}
+        avatar={avatar}
+        phone={phone}
+      />
+    );
+  };
+
+  render() {
+    const { loading, contacts, error } = this.state;
+
+    const contactsSorted = contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+    return (
+      <View style={styles.container}>
+        {loading && <ActivityIndicator size="large" />}
+        {error && <Text>Error...</Text>}
+        {!loading && !error && (
+          <FlatList
+            data={contactsSorted}
+            keyExtractor={keyExtractor}
+            renderItem={this.renderContact}
+          />
+        )}
+      </View>
+    );
+  }
+}
+
+export default Contacts;
